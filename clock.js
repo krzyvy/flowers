82af629af47bea98760016574902b7ea01d6722a
krzyvy <118526617+krzyvy@users.noreply.github.com>
2024-12-23 20:38:11 +0100

    Add files via upload

--- clock.js
+++ clock.js
@@ -6,12 +6,13 @@ var x = setInterval(function() {
     var distance = now - countdownDate;
 
     //var years = Math.floor(distance / (1000 * 60 * 60 * 24 * 365));
+    var mount = Math.floor(distance / (1000 * 60 * 60 * 24 * 30.436875));
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    document.getElementById("countdown").innerHTML = "<div class='title'>Tyle zemną wytrzymałaś:</div>"+ "<div>"+days + " dni </div> " + "<div>"+ hours + " godzin </div> " + "<div>"+ minutes + " minut</div>" + "<div>"+ seconds + " sekund </div> <div>GRATULACJE!</div>";
+    document.getElementById("countdown").innerHTML = "<div class='title'>Tyle zemna wytrzymalas:</div> <div>roku jeszcze nie wytrzymalas</div> <div>"+ mount+" miesiace</div>"+  "<div>"+days + " dni </div> " + "<div>"+ hours + " godzin </div> " + "<div>"+ minutes + " minut</div>" + "<div>"+ seconds + " sekund </div> <div>GRATULACJE!</div>";
 }, 1000);
 
 const one = document.querySelector('#one');
